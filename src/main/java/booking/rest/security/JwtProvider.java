0162a6de5a89857058395ccaa889ef5537ce3e06
oikkarok <oikkarok>
2022-03-27 22:34:56 +0200

    security

---
+++ src/main/java/booking/rest/security/JwtProvider.java
@@ -0,0 +1,62 @@
+package booking.rest.security;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import org.joda.time.DateTime;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTCreator;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class JwtProvider {
+
+    private static final Logger log = LoggerFactory.getLogger(JwtProvider.class);
+    private static final String issuer = "demo-service";
+
+    /**
+     * Create jwt string.
+     *
+     * @param subject       the subject
+     * @param payloadClaims the payload claims
+     * @return the JWT string
+     */
+    public static String createJwt(String subject, List<Entry> payloadClaims) {
+       
+	JWTCreator.Builder builder =  JWT.create()
+                .withSubject(subject)
+                .withIssuer(issuer)
+                .withIssuedAt(DateTime.now().toDate())
+                .withExpiresAt(DateTime.now().plusMonths(1).toDate());
+
+        if (payloadClaims != null && !payloadClaims.isEmpty()) {
+            
+            for (Entry entry : payloadClaims) {
+                builder.withClaim(entry.getKey().toString(), entry.getValue().toString());
+            }
+            
+        } else {
+            log.warn("You are building a JWT without header claims!");
+        }
+        
+        return builder.sign(Algorithm.HMAC256(SecurityConfig.secret));
+    }
+
+    /**
+     * Verify jwt decoded.
+     *
+     * @param jwt the JWT string
+     * @return the decoded JWT
+     */
+    public static DecodedJWT verifyJwt(String jwt) {
+        return JWT.require(Algorithm.HMAC256(SecurityConfig.secret)).build().verify(jwt);
+    }
+
+    private JwtProvider() {}
+}
--- src/main/java/booking/rest/security/SecurityConfig.java
+++ src/main/java/booking/rest/security/SecurityConfig.java
@@ -1,5 +1,36 @@
 package booking.rest.security;
 
-public class SecurityConfig {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    public static String secret;
+    public static String param;
+    public static String prefix;
+
+    @Autowired
+    public SecurityConfig(Environment env) {
+        SecurityConfig.secret = env.getProperty("security.secret");
+        SecurityConfig.param = env.getProperty("security.param");
+        SecurityConfig.prefix = env.getProperty("security.prefix");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.cors().and().csrf().disable();
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and().addFilter(new AuthorizationFilter(authenticationManager())).authorizeRequests()
+                .antMatchers("/public/**", "/v2/api-docs", "/configuration/**", "/swagger*/**", "/webjars/**").permitAll()
+                .anyRequest().authenticated();
+    }
 }
